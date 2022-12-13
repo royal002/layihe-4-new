328ffbd72fc26bb5054d325cc847a1a61ee332e3
royal002 <115249056+royal002@users.noreply.github.com>
2022-12-13 17:20:37 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Movies from "./Movies";
+import Organizer from "./components/Organizer";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>FilmBucket</h1>
+      <Movies />
+    </div>
+  );
+}
+
